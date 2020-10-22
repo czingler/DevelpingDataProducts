07a50aafceca0cad2356eb8c05bc3147b1be603d
CHARLES ZINGLER <charles@zingler.biz>
2020-10-22 15:39:20 +1100

    Add files via upload

--- server.R
+++ server.R
@@ -1,7 +1,30 @@
 library(shiny)
+library(tidyverse)
+
+data("uspop")
+dfpop <- data.frame(time(uspop), uspop)
+names(dfpop)<- c("year","pop")
+newpop<- data.frame(c(1980,1990,2000,2010),
+                    c(226.5,250.1,282.2,309.3))
+##  United States Census Bureau
+names(newpop)<- c("year","pop")
+dfnpop<- add_row(dfpop,newpop)
+pop <- subset(dfnpop,dfnpop$year>1890)
+
 shinyServer(function(input, output) {
+    lmpop<- lm(log(pop)~year,pop)
+   
+    modpred <- reactive({
+     SI <-  input$slider
+     predict(lmpop,newdata = data.frame(year=SI))
+    })
     
-    output$text = renderText(input$slider)
+    output$text <- renderText({exp(modpred())})
+    
+    output$plot <- renderPlot({plot(pop$year,log(pop$pop), xlab = "Year", ylab= "log of Population", main = "Regression Generating data")})
+    
     
     }   
 )
+   
+    
--- ui.R
+++ ui.R
@@ -2,17 +2,20 @@ library(shiny)
 
 
 shinyUI(fluidPage(
-    titlePanel("All thing great and small"),
+    titlePanel("How will the US population increase based on recent population trajectory from 1900"),
     sidebarLayout(
         sidebarPanel(
-             h3("let's slide stuff"),
-             sliderInput("slider"," slide it",0,500,40)
+             h3("Use the slider to pick the forcast year of choice"),
+             
+             sliderInput("slider"," slide it",1970,2300,2019)
            ),
        
         mainPanel(
-            h3("this is the slider value"),
-            strong(em("this is a neat app")),
-            textOutput("text")
+            h3("This is the forcast population! Based on the year selected!"),
+            h3("(The base linear regression of the log of the population value has an adjusted R-squared of 0.9944)"),
+            strong(em("This result is the forcast population in millions")),
+            h1(textOutput("text")),
+            plotOutput("plot")
             )
         )
     )
